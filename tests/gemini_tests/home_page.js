a3d86fa6ae2d48f0d6458ed52c59cf401a7cca3a
Nazar <nazarky@ukr.net>
2014-11-12 13:06:18 +0200

    #132: updated gitignore file.

--- tests/gemini_tests/home_page.js
+++ tests/gemini_tests/home_page.js
@@ -1,12 +1,12 @@
 var gemini = require('gemini');
 
 gemini.suite('buttons', function(suite) {
-    suite.setUrl('/')
-        .setCaptureElements('.button')
-        .capture('plain')
-        .capture('hovered', function(actions) {
-            actions.mouseMove('.button');
-        }).capture('pressed', function(actions, find) {
-            actions.mouseDown(find('.button'));
-        });
+  suite.setUrl('/')
+    .setCaptureElements('.button')
+    .capture('plain')
+    .capture('hovered', function(actions) {
+        actions.mouseMove('.button');
+    }).capture('pressed', function(actions, find) {
+        actions.mouseDown(find('.button'));
+    });
 });
