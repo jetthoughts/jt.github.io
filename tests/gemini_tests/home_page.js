3230f27b6f0f877b3610fdfe74c1ebf981b0ad8e
Nazar <nazarky@ukr.net>
2014-11-12 13:16:41 +0200

    #132: updated test for buttons on home page.

--- tests/gemini_tests/home_page.js
+++ tests/gemini_tests/home_page.js
@@ -3,10 +3,13 @@ var gemini = require('gemini');
 gemini.suite('buttons', function(suite) {
   suite.setUrl('/')
     .setCaptureElements('.button')
+    .before(function(actions, find) {
+      this.button = find('.button');
+    })
     .capture('plain')
-    .capture('hovered', function(actions) {
-        actions.mouseMove('.button');
+    .capture('hovered', function(actions, find) {
+        actions.mouseMove(this.button);
     }).capture('pressed', function(actions, find) {
-        actions.mouseDown(find('.button'));
+        actions.mouseDown(this.button);
     });
 });
