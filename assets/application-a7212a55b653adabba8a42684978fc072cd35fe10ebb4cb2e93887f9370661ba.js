a456c54f1f988bdf93c6e26a153f0647101744d4
Andriy Parashchuk <anrey.andreevich@gmail.com>
2018-04-26 14:12:36 +0300

    Update master branch

--- assets/application-a7212a55b653adabba8a42684978fc072cd35fe10ebb4cb2e93887f9370661ba.js
+++ assets/application-a7212a55b653adabba8a42684978fc072cd35fe10ebb4cb2e93887f9370661ba.js
@@ -1,103 +1,100 @@
 $(document).ready(function () {
   var contactForm = $('.contact-form'),
-    submitBtn = $('.contact-form__btn'),
-    requiredFields = $('[required]');
+      submitBtn = $('.contact-form__btn'),
+      requiredFields = $('[required]');
 
-  function postToGoogle(form, name, email, message, source) {
-    var params = {
-      data: {
-        'entry.408156996': name,
-        'entry.1818089031': email,
-        'entry.1509418088': message,
-        'entry.1693009463': source
-      },
-      type: 'POST',
-      dataType: 'xml'
-    };
+    function postToGoogle(form, name, email, message, source) {
+      var params = {
+        data: {
+          'entry.408156996': name,
+          'entry.1818089031': email,
+          'entry.1509418088': message,
+          'entry.1693009463': source
+        },
+        type: 'POST',
+        dataType: 'xml'
+      };
 
-    fetch('https://docs.google.com/forms/d/19gksTm0W2HhbBV1jmaHO9O4wPGoO0KC2zEZkrCIwKsg/formResponse', params)
+      fetch('https://docs.google.com/forms/d/19gksTm0W2HhbBV1jmaHO9O4wPGoO0KC2zEZkrCIwKsg/formResponse', params)
       .then(function (res) {
         return res.json()
       })
       .then(function (jsn) {
         formHasSent(form)
       });
-    formHasSent(form);
-  }
+      formHasSent(form);
+    }
 
   function formHasSent(form) {
     $('#form-success').fadeIn().siblings('.modal').fadeOut();
   }
 
-    function validateEmail(email) {
-      var val = email.val(),
-        pat = new RegExp(/.+@.+\..+/i);
-      return pat.test(val);
-    }
+  function validateEmail(email) {
+    var val = email.val(),
+    pat = new RegExp(/.+@.+\..+/i);
+    return pat.test(val);
+  }
 
-    function requiredInput(input) {
-      var val = input.val();
-      return val.length > 0;
-    }
+  function requiredInput(input) {
+    var val = input.val();
+    return val.length > 0;
+  }
 
-    requiredFields.keyup(function () {
-      var self = $(this),
-        form = self.parents('.contact-form');
-      checkRequired(self);
-      checkValidity(form);
-    });
+  requiredFields.keyup(function () {
+    var self = $(this),
+    form = self.parents('.contact-form');
+    checkRequired(self);
+    checkValidity(form);
+  });
 
-    requiredFields.on('blur', function () {
-      var self = $(this),
-        form = self.parents('.contact-form');
-      checkRequired(self);
-      checkValidity(form);
-    });
+  requiredFields.on('blur', function () {
+    var self = $(this),
+    form = self.parents('.contact-form');
+    checkRequired(self);
+    checkValidity(form);
+  });
 
-    function checkValidity(form) {
-      var _email = form.find('.email'),
-        _message = form.find('.message-txt');
-      form.toggleClass('invalid', !(validateEmail(_email) && requiredInput(_message)));
-    }
+  function checkValidity(form) {
+    var _email = form.find('.email'),
+    _message = form.find('.message-txt');
+    form.toggleClass('invalid', !(validateEmail(_email) && requiredInput(_message)));
+  }
 
-    contactForm.each(function () {
+  contactForm.each(function () {
+    var self = $(this);
+    checkValidity(self);
+  });
+
+  function checkRequired(el) {
+    $el = (typeof el !== 'undefined') ? $(el) : $('[required]');
+    $el.each(function () {
       var self = $(this);
-      checkValidity(self);
-    });
-
-    function checkRequired(el) {
-
-      $el = (typeof el !== 'undefined') ? $(el) : $('[required]');
-      $el.each(function () {
-        var self = $(this);
-        if (!requiredInput(self)) {
-          self.parent().toggleClass('invalid', !requiredInput(self));
-        } else if (self.attr('type') === 'email') {
-          self.parent().toggleClass('invalid', !validateEmail(self));
-        } else {
-          self.parent().toggleClass('invalid', !requiredInput(self));
-        }
-      });
-
-    }
-
-    submitBtn.on('click', function (e) {
-      e.preventDefault();
-      var self = $(this),
-        form = self.parents('.contact-form:not(.invalid)'),
-        requiredInputs = self.parents('form').find('[required]'),
-        _name = form.find('.name').val(),
-        _email = form.find('.email').val(),
-        _message = form.find('.message-txt').val(),
-        _source = form.data('source');
-      if (form.length) {
-        postToGoogle(form, _name, _email, _message, _source);
+      if (!requiredInput(self)) {
+        self.parent().toggleClass('invalid', !requiredInput(self));
+      } else if (self.attr('type') === 'email') {
+        self.parent().toggleClass('invalid', !validateEmail(self));
       } else {
-        checkRequired(requiredInputs);
+        self.parent().toggleClass('invalid', !requiredInput(self));
       }
     });
   }
-  );
+
+  submitBtn.on('click', function (e) {
+    e.preventDefault();
+    var self = $(this),
+    form = self.parents('.contact-form:not(.invalid)'),
+    requiredInputs = self.parents('form').find('[required]'),
+    _name = form.find('.name').val(),
+    _email = form.find('.email').val(),
+    _message = form.find('.message-txt').val(),
+    _source = form.data('source');
+    if (form.length) {
+      postToGoogle(form, _name, _email, _message, _source);
+    } else {
+      checkRequired(requiredInputs);
+    }
+  });
+});
 
 $(document).ready(function () {
   // scroll animation
@@ -147,6 +144,7 @@ $(document).ready(function () {
   }
 
   if ($('.uses-cases-slideshow').length) {
+    var usesCasesState = { isPlay: false }
     var $usesCasesSlideshow = $('.uses-cases-slideshow');
 
     $usesCasesSlideshow.on('init', function(event, slick, currentSlide, nextSlide) {
@@ -171,7 +169,7 @@ $(document).ready(function () {
         {
           breakpoint: 768,
           settings: {
-            dots: true
+            arrows: true
           }
         },
       ]
@@ -220,7 +218,7 @@ $(document).ready(function () {
   });
 
   // scroll go to
-  $('.js-scroll').on('click', function(e){
+  $('.js-scroll').on('click', function(e) {
     var href =  $(this).attr('href');
     $('html, body').stop().animate({ 
       scrollTop: href === "#" ? 0 : $(href).offset().top
@@ -228,7 +226,7 @@ $(document).ready(function () {
     e.preventDefault();
   });
 
-  $(window).scroll( function(){
+  $(window).scroll( function() {
     $('.section, .wave-wrapper').each(function() {
       var $this = $(this);
       var bottom_of_object = $this.offset().top + $this.outerHeight() / 5;
@@ -236,14 +234,15 @@ $(document).ready(function () {
 
       if (bottom_of_window > bottom_of_object) {
         $this.addClass('animation');
-        if ($this.hasClass('uses-cases')) {
+        if ($this.hasClass('uses-cases') && !usesCasesState.isPlay) {
+          usesCasesState.isPlay = true;
           $('.uses-cases-slideshow').slick('slickPlay');
         }
       }
     });
   });
 
-  document.onkeydown = function(e){
+  document.onkeydown = function(e) {
     if (e.keyCode == 32 && !$('body.modal-open').length) {
       $('.section').each(function() {
           var windowHeight = $(window).height();
@@ -251,12 +250,12 @@ $(document).ready(function () {
           var windowTop = $(window).scrollTop();
 
           if (windowTop < thisOffset) {
-            $('html, body').stop().animate({ 
+            $('html, body').stop().animate({
               scrollTop: thisOffset
             }, 300);
             return false
           } else {
-            $('html, body').stop().animate({ 
+            $('html, body').stop().animate({
               scrollTop: (windowTop + windowHeight)
             }, 250);
           }
