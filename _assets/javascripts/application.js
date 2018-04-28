cb71197e8b4b5dfece90bc481daea70f9dec2c3e
Paul Nikitochkin <pftg@users.noreply.github.com>
2018-04-28 22:06:20 +0300

    Optimized loading

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -167,9 +167,9 @@ $(document).ready(function () {
       e.preventDefault();
     };
   }
-});
 
-document.addEventListener("DOMContentLoaded", function() {
+
+
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
   var active = false;
 
@@ -179,25 +179,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
       setTimeout(function() {
         lazyImages.forEach(function(lazyImage) {
-          if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
             lazyImage.src = lazyImage.dataset.src;
             lazyImage.srcset = lazyImage.dataset.srcset;
             lazyImage.classList.remove("lazy");
 
-            lazyImages = lazyImages.filter(function(image) {
-              return image !== lazyImage;
-            });
-
-            if (lazyImages.length === 0) {
-              document.removeEventListener("scroll", lazyLoad);
-              window.removeEventListener("resize", lazyLoad);
-              window.removeEventListener("orientationchange", lazyLoad);
-            }
-          }
+		  document.removeEventListener("scroll", lazyLoad);
+		  window.removeEventListener("resize", lazyLoad);
+		  window.removeEventListener("orientationchange", lazyLoad);
         });
 
         active = false;
-      }, 200);
+      }, 500);
     }
   };
 
