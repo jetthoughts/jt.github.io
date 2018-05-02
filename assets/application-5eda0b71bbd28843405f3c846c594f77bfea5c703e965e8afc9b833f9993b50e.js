a3cba441e01aa8e0a06e707a234fef9315f659a9
andriyParashchuk <anrey.andreevich@gmail.com>
2018-05-02 12:12:27 +0000

    Merge pull request #286 from jetthoughts/286-loading-custom-fonts
    Support Proprietary Fonts from Design but optimize loading

--- assets/application-5eda0b71bbd28843405f3c846c594f77bfea5c703e965e8afc9b833f9993b50e.js
+++ assets/application-5eda0b71bbd28843405f3c846c594f77bfea5c703e965e8afc9b833f9993b50e.js
@@ -264,8 +264,6 @@ $(document).ready(function () {
     };
   }
 
-
-
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
   var active = false;
 
@@ -275,13 +273,13 @@ $(document).ready(function () {
 
       setTimeout(function() {
         lazyImages.forEach(function(lazyImage) {
-            lazyImage.src = lazyImage.dataset.src;
-            lazyImage.srcset = lazyImage.dataset.srcset;
-            lazyImage.classList.remove("lazy");
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
 
-		  document.removeEventListener("scroll", lazyLoad);
-		  window.removeEventListener("resize", lazyLoad);
-		  window.removeEventListener("orientationchange", lazyLoad);
+          document.removeEventListener("scroll", lazyLoad);
+          window.removeEventListener("resize", lazyLoad);
+          window.removeEventListener("orientationchange", lazyLoad);
         });
 
         active = false;
@@ -292,4 +290,10 @@ $(document).ready(function () {
   document.addEventListener("scroll", lazyLoad);
   window.addEventListener("resize", lazyLoad);
   window.addEventListener("orientationchange", lazyLoad);
+});
+
+var font = new FontFaceObserver('Graphik Web');
+
+font.load().then(function () {
+  document.documentElement.className += " fonts-loaded";
 });
